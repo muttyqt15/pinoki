d0538d8a8f67ab3c9e789b6482a48acad32d59d9
muttyqt15 <95131963+muttyqt15@users.noreply.github.com>
2024-01-09 23:46:31 +0700

    fix: fixed next/babel eslint error by changing .eslintrc.json

---
+++ pinoki-web/src/components/Form.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React, { useState } from "react";
+import { FormProps } from "@/types/formInterfaces"
+import FormItem from "./FormItem";
+import { cn } from "@/lib/utils";
+
+const Form = (p: FormProps & { className?: string } & { buttonPrompt: string }) => {
+  const getInitialState = () => {
+    let initialState: any = {};
+    p.fields.forEach((field) => {
+      initialState[field.property] = field.value;
+    });
+    return initialState;
+  };
+  const [state, setState] = useState(getInitialState());
+  const [error, setError] = useState(false);
+  const handleChange = (property: string, value: string | boolean) => {
+    setState((state: any) => ({ ...state, [property]: value }));
+  };
+
+  const handleSubmit = () => {
+    // // Check for missing required fields before submission
+    // const missingFields = p.fields.filter(
+    //   (field) => field.required && !state[field.property]
+    // );
+
+    // if (missingFields.length > 0) {
+    //   // Handle missing required fields (you can show an error, prevent submission, etc.)
+    //   console.error(
+    //     "Missing required fields:",
+    //     missingFields.map((field) => field.property)
+    //   );
+    //   setError(true);
+    // } else {
+    //   // All required fields are filled, proceed with form submission
+      p.onSubmit(state);
+      setState(getInitialState());
+    // }
+  };
+  return (
+    <>
+      <section className={cn("max-w-72 p-4 rounded-xl", p.className)}>
+        <form className="flex flex-col">
+          {p.fields.map((field, i) => {
+            return (
+              <FormItem
+                key={i}
+                {...field}
+                onChange={handleChange}
+                value={state[field.property]}
+                className="m-2"
+              />
+            );
+          })}
+        </form>
+        <div className="flex justify-between items-center mt-4">
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="blue-btn">
+            {p.buttonPrompt}
+          </button>
+          {error ? (
+            <p className="text-red-400 font-bold tracking-tighter">Fill in all forms!</p>
+          ) : (
+            <p></p>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Form;
---
+++ pinoki-web/src/components/FormItem.tsx
@@ -0,0 +1,34 @@
+import React, { ChangeEvent } from "react";
+import { FormItemProps, OutputProps } from "@/types/formInterfaces";
+import { cn } from "@/lib/utils"
+
+function FormItem(p: FormItemProps & OutputProps & { className?: string }) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    p.onChange(p.property, e.target.value);
+  };
+  return (
+    <>
+      <label htmlFor={p.property} className="signika">
+        {p.name}
+        {p.required && <span className="text-red-500 text-xs"> *</span>}
+      </label>
+      <input
+        type={p.type}
+        name={p.name}
+        placeholder={p.placeholder}
+        id={p.property}
+        value={p.value as string}
+        checked={p.value as boolean} // Checks if form field is filled out
+        required={p.required}
+        onChange={handleChange}
+        autoComplete="off"
+        className={cn(
+          "border-2 border-gray-300 focus:border-blue-500 px-2 py-1 rounded-xl animate-ease",
+          p.className
+        )}
+      />
+    </>
+  );
+}
+
+export default FormItem;
