f0570a5e97ceabe5c707f52ca9d587edf1dca731
muttyqt15 <95131963+muttyqt15@users.noreply.github.com>
2024-01-16 18:13:24 +0700

    test

--- pinoki-extension/src/components/buttons/LaporkanHoax.tsx
+++ pinoki-extension/src/components/buttons/LaporkanHoax.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LaporkanHoaxButton = ({ handleClick }: { handleClick: () => void }) => {
   return (
     <button
-      className="text-white border bg-[#35D78E] flex items-center justify-center text-2xl rounded-lg gap-3 w-full py-1 px-2 transition transform duration-300 ease-in-out hover:bg-green-400"
+      className="text-white border shadow-inner bg-[#35D78E] flex items-center justify-center text-2xl rounded-lg gap-3 w-full py-1 px-2 transition transform duration-300 ease-in-out hover:bg-green-400"
       onClick={handleClick}>
       Laporkan hoax
       <span>
